302de7b294e686461c143d95f04f065248be6052
Benoit Devos <benoit@logion.network>
2024-02-28 17:44:18 +0100

    chore: remove useless migration.
    logion-network/logion-internal#488

--- pallet-lo-authority-list/src/migrations.rs
+++ pallet-lo-authority-list/src/migrations.rs
@@ -1,29 +1,9 @@
 use frame_support::traits::Get;
 use frame_support::weights::Weight;
-use frame_support::traits::OnRuntimeUpgrade;
 
 use crate::{Config, PalletStorageVersion, pallet::StorageVersion};
 
-pub mod v4 {
-    use super::*;
-    use crate::*;
-
-    pub struct Dummy<T>(sp_std::marker::PhantomData<T>);
-    impl<T: Config> OnRuntimeUpgrade for Dummy<T> {
-
-        fn on_runtime_upgrade() -> Weight {
-            super::do_storage_upgrade::<T, _>(
-                StorageVersion::V4Region,
-                StorageVersion::V4Region,
-                "Dummy",
-                || {
-                    LegalOfficerSet::<T>::translate_values(|legal_officer_data: LegalOfficerDataOf<T>| { Some(legal_officer_data) } );
-                }
-            )
-        }
-    }
-}
-
+#[allow(dead_code)]
 fn do_storage_upgrade<T: Config, F>(expected_version: StorageVersion, target_version: StorageVersion, migration_name: &str, migration: F) -> Weight
 where F: FnOnce() -> () {
     let storage_version = PalletStorageVersion::<T>::get();
